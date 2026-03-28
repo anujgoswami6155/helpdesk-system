c818b3797d4a474aedb17d5284127368c0c6e508
Anujgiri Goswami <anujgoswami0212@gmail.com>
2026-03-28 17:50:05 +0530

    Fix: Password visibilty in JSON file while creating a ticket

--- src/main/java/com/helpdesk/entity/User.java
+++ src/main/java/com/helpdesk/entity/User.java
@@ -1,5 +1,6 @@
 package com.helpdesk.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.helpdesk.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
